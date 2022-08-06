340b4920397e61b7103aa4e1100025cf6d0adbeb
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-08-05 22:34:22 -0500

    Initial Commit

---
+++ Birthday/BirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayApp.swift
+//  Birthday
+//
+//  Created by Renato Oliveira Fraga on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
